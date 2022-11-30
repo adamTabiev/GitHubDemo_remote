bd126f108d6f5489f3fddf98eb5977dc59301308
adamTab <tabievadam@gmail.com>
2022-11-30 19:26:06 +0100

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Адам Табиев on 30.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
